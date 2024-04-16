0bed1a68f047b81729405f2a4eaec7e7491f0e7a
Hemel509156 <ahashanhabibhemel@gmail.com>
2024-04-16 23:55:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.clock;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.clock.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
